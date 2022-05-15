bfec5b634f29e61334b3b101440a530fbf315002
faizannsethi <faizansethi555@gmail.com>
2022-05-15 21:59:36 +0500

    Message Info UI Added

--- app/src/main/java/com/example/esap/MessageInfo.java
+++ app/src/main/java/com/example/esap/MessageInfo.java
@@ -26,204 +26,204 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MessageInfo extends AppCompatActivity {
-    private String phoneNo;
-    private String phoneNoSrc;
-    private String message;
-    String service;
-    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
-    private FirebaseFirestore db;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_message_info);
-        db = FirebaseFirestore.getInstance();
-        Intent intent = getIntent();
-        service = intent.getStringExtra("key");
-        String[] splitService= service.split(" ");
-        TextView msg = findViewById(R.id.textView9);
-
-        if(splitService[0].equals("ambulance")){
-            msg.setText("آپ کی درخواست درج کر لی گئی ہے۔ آپ کی مدد کے لئے ایمبولینس جلد بھیج دی جائے گی۔");
-        }
-        if(splitService[0].equals("firebrigade")){
-            msg.setText("آپ کی درخواست درج کر لی گئی ہے۔ آپ کی مدد کے لئے فائیربریگیڈ جلد بھیج دی جائے گی۔");
-        }
-        double latitude = intent.getDoubleExtra("key2", 0.0);
-        double longitude = intent.getDoubleExtra("key3", 0.0);
-        TextView location = findViewById(R.id.textView13);
-        location.setText("Lat: "+ latitude +"   Lon: "+longitude);
-        String text = intent.getStringExtra("key4");
-        TextView userMsg = findViewById(R.id.textView11);
-        userMsg.setText(text);
-
-        addDataToFirestore(text,splitService[0] );
-
-        Thread thread1 = new Thread(){
-
-            public void run() {
-                try {
-                    sleep(1000);
-                }
-                catch(Exception e) {
-                    e.printStackTrace();
-                }
-                finally {
-                    sendSMSMessage();
-
-                }
-            }
-        };
-
-        Thread thread = new Thread(){
-
-            public void run() {
-                try {
-                    sleep(3000);
-                }
-                catch(Exception e) {
-                    e.printStackTrace();
-                }
-                finally {
-
-                    Intent intent = new Intent(MessageInfo.this, HelpImages.class);
-                    intent.putExtra("key", service);
-                    startActivity(intent);
-                }
-            }
-        };
-        thread1.start();
-        thread.start();
-
-
-
-    }
-    protected void sendSMSMessage(){
-        phoneNo = "03155903128";
-        phoneNoSrc = "03155903128";
-        message = "ambulance";
-
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.SEND_SMS)
-                != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.SEND_SMS)) {
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.SEND_SMS},
-                        MY_PERMISSIONS_REQUEST_SEND_SMS);
-            }
-        }
-        else{
-            String SENT = "SMS_SENT";
-            String DELIVERED = "SMS_DELIVERED";
-
-            PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
-                    new Intent(SENT), 0);
-
-            PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
-                    new Intent(DELIVERED), 0);
-
-            //---when the SMS has been sent---
-            registerReceiver(new BroadcastReceiver(){
-                @Override
-                public void onReceive(Context context, Intent intent) {
-
-                    switch (getResultCode())
-                    {
-                        case Activity.RESULT_OK:
-                            Toast.makeText(getBaseContext(), "SMS sent",
-                                    Toast.LENGTH_SHORT).show();
-                            break;
-                        case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-                            Toast.makeText(getBaseContext(), "Generic failure",
-                                    Toast.LENGTH_SHORT).show();
-                            break;
-                        case SmsManager.RESULT_ERROR_NO_SERVICE:
-                            Toast.makeText(getBaseContext(), "No service",
-                                    Toast.LENGTH_SHORT).show();
-                            break;
-                        case SmsManager.RESULT_ERROR_NULL_PDU:
-                            Toast.makeText(getBaseContext(), "Null PDU",
-                                    Toast.LENGTH_SHORT).show();
-                            break;
-                        case SmsManager.RESULT_ERROR_RADIO_OFF:
-                            Toast.makeText(getBaseContext(), "Radio off",
-                                    Toast.LENGTH_SHORT).show();
-                            break;
-                    }
-                }
-            }, new IntentFilter(SENT));
-
-            //---when the SMS has been delivered---
-            registerReceiver(new BroadcastReceiver(){
-                @Override
-                public void onReceive(Context arg0, Intent arg1) {
-                    switch (getResultCode())
-                    {
-                        case Activity.RESULT_OK:
-                            Toast.makeText(getBaseContext(), "SMS delivered",
-                                    Toast.LENGTH_SHORT).show();
-                            break;
-                        case Activity.RESULT_CANCELED:
-                            Toast.makeText(getBaseContext(), "SMS not delivered",
-                                    Toast.LENGTH_SHORT).show();
-                            break;
-                    }
-                }
-            }, new IntentFilter(DELIVERED));
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(phoneNo, phoneNoSrc, message,sentPI, deliveredPI);
-
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    SmsManager smsManager = SmsManager.getDefault();
-                    smsManager.sendTextMessage(phoneNo, null, message, null, null);
-                    Toast.makeText(getApplicationContext(), "SMS sent.",
-                            Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getApplicationContext(),
-                            "SMS faild, please try again.", Toast.LENGTH_LONG).show();
-                    return;
-                }
-            }
-        }
-
-    }
-
-    private void addDataToFirestore(String user_msg, String resp) {
-
-        // creating a collection reference
-        // for our Firebase Firestore database.
-        CollectionReference dbCourses = db.collection("user_msg_resp");
-
-        // adding our data to our courses object class.
-        firebase_msg_resp data = new firebase_msg_resp(user_msg, resp);
-
-        // below method is use to add data to Firebase Firestore.
-        dbCourses.add(data).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-            @Override
-            public void onSuccess(DocumentReference documentReference) {
-                // after the data addition is successful
-                // we are displaying a success toast message.
-                Toast.makeText(MessageInfo.this, "Your Data has been added to Firebase Firestore", Toast.LENGTH_SHORT).show();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                // this method is called when the data addition process is failed.
-                // displaying a toast message when data addition is failed.
-                Toast.makeText(MessageInfo.this, "Fail to add data \n" + e, Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
+//    private String phoneNo;
+//    private String phoneNoSrc;
+//    private String message;
+//    String service;
+//    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
+//    private FirebaseFirestore db;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_message_info);
+//        db = FirebaseFirestore.getInstance();
+//        Intent intent = getIntent();
+//        service = intent.getStringExtra("key");
+//        String[] splitService= service.split(" ");
+////        TextView msg = findViewById(R.id.textView9);
+//
+//        if(splitService[0].equals("ambulance")){
+//         //   msg.setText("آپ کی درخواست درج کر لی گئی ہے۔ آپ کی مدد کے لئے ایمبولینس جلد بھیج دی جائے گی۔");
+//        }
+//        if(splitService[0].equals("firebrigade")){
+//         //   msg.setText("آپ کی درخواست درج کر لی گئی ہے۔ آپ کی مدد کے لئے فائیربریگیڈ جلد بھیج دی جائے گی۔");
+//        }
+//        double latitude = intent.getDoubleExtra("key2", 0.0);
+//        double longitude = intent.getDoubleExtra("key3", 0.0);
+////        TextView location = findViewById(R.id.textView13);
+////        location.setText("Lat: "+ latitude +"   Lon: "+longitude);
+////        String text = intent.getStringExtra("key4");
+////        TextView userMsg = findViewById(R.id.textView11);
+//       // userMsg.setText(text);
+//
+//        //addDataToFirestore(text,splitService[0] );
+//
+//        Thread thread1 = new Thread(){
+//
+//            public void run() {
+//                try {
+//                    sleep(1000);
+//                }
+//                catch(Exception e) {
+//                    e.printStackTrace();
+//                }
+//                finally {
+//                    sendSMSMessage();
+//
+//                }
+//            }
+//        };
+//
+//        Thread thread = new Thread(){
+//
+//            public void run() {
+//                try {
+//                    sleep(3000);
+//                }
+//                catch(Exception e) {
+//                    e.printStackTrace();
+//                }
+//                finally {
+//
+//                    Intent intent = new Intent(MessageInfo.this, HelpImages.class);
+//                    intent.putExtra("key", service);
+//                    startActivity(intent);
+//                }
+//            }
+//        };
+//        thread1.start();
+//        thread.start();
+//
+//
+//
+//    }
+//    protected void sendSMSMessage(){
+//        phoneNo = "03155903128";
+//        phoneNoSrc = "03155903128";
+//        message = "ambulance";
+//
+//        if (ContextCompat.checkSelfPermission(this,
+//                Manifest.permission.SEND_SMS)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+//                    Manifest.permission.SEND_SMS)) {
+//            } else {
+//                ActivityCompat.requestPermissions(this,
+//                        new String[]{Manifest.permission.SEND_SMS},
+//                        MY_PERMISSIONS_REQUEST_SEND_SMS);
+//            }
+//        }
+//        else{
+//            String SENT = "SMS_SENT";
+//            String DELIVERED = "SMS_DELIVERED";
+//
+//            PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
+//                    new Intent(SENT), 0);
+//
+//            PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
+//                    new Intent(DELIVERED), 0);
+//
+//            //---when the SMS has been sent---
+//            registerReceiver(new BroadcastReceiver(){
+//                @Override
+//                public void onReceive(Context context, Intent intent) {
+//
+//                    switch (getResultCode())
+//                    {
+//                        case Activity.RESULT_OK:
+//                            Toast.makeText(getBaseContext(), "SMS sent",
+//                                    Toast.LENGTH_SHORT).show();
+//                            break;
+//                        case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+//                            Toast.makeText(getBaseContext(), "Generic failure",
+//                                    Toast.LENGTH_SHORT).show();
+//                            break;
+//                        case SmsManager.RESULT_ERROR_NO_SERVICE:
+//                            Toast.makeText(getBaseContext(), "No service",
+//                                    Toast.LENGTH_SHORT).show();
+//                            break;
+//                        case SmsManager.RESULT_ERROR_NULL_PDU:
+//                            Toast.makeText(getBaseContext(), "Null PDU",
+//                                    Toast.LENGTH_SHORT).show();
+//                            break;
+//                        case SmsManager.RESULT_ERROR_RADIO_OFF:
+//                            Toast.makeText(getBaseContext(), "Radio off",
+//                                    Toast.LENGTH_SHORT).show();
+//                            break;
+//                    }
+//                }
+//            }, new IntentFilter(SENT));
+//
+//            //---when the SMS has been delivered---
+//            registerReceiver(new BroadcastReceiver(){
+//                @Override
+//                public void onReceive(Context arg0, Intent arg1) {
+//                    switch (getResultCode())
+//                    {
+//                        case Activity.RESULT_OK:
+//                            Toast.makeText(getBaseContext(), "SMS delivered",
+//                                    Toast.LENGTH_SHORT).show();
+//                            break;
+//                        case Activity.RESULT_CANCELED:
+//                            Toast.makeText(getBaseContext(), "SMS not delivered",
+//                                    Toast.LENGTH_SHORT).show();
+//                            break;
+//                    }
+//                }
+//            }, new IntentFilter(DELIVERED));
+//            SmsManager smsManager = SmsManager.getDefault();
+//            smsManager.sendTextMessage(phoneNo, phoneNoSrc, message,sentPI, deliveredPI);
+//
+//        }
+//    }
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        switch (requestCode) {
+//            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+//                if (grantResults.length > 0
+//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    SmsManager smsManager = SmsManager.getDefault();
+//                    smsManager.sendTextMessage(phoneNo, null, message, null, null);
+//                    Toast.makeText(getApplicationContext(), "SMS sent.",
+//                            Toast.LENGTH_LONG).show();
+//                } else {
+//                    Toast.makeText(getApplicationContext(),
+//                            "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+//                    return;
+//                }
+//            }
+//        }
+//
+//    }
+//
+//    private void addDataToFirestore(String user_msg, String resp) {
+//
+//        // creating a collection reference
+//        // for our Firebase Firestore database.
+//        CollectionReference dbCourses = db.collection("user_msg_resp");
+//
+//        // adding our data to our courses object class.
+//        firebase_msg_resp data = new firebase_msg_resp(user_msg, resp);
+//
+//        // below method is use to add data to Firebase Firestore.
+//        dbCourses.add(data).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//            @Override
+//            public void onSuccess(DocumentReference documentReference) {
+//                // after the data addition is successful
+//                // we are displaying a success toast message.
+//                Toast.makeText(MessageInfo.this, "Your Data has been added to Firebase Firestore", Toast.LENGTH_SHORT).show();
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                // this method is called when the data addition process is failed.
+//                // displaying a toast message when data addition is failed.
+//                Toast.makeText(MessageInfo.this, "Fail to add data \n" + e, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//    }
 
 }
