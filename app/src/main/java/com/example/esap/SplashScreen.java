4044820d60dad79fc1ea0edae1458f639c87fd77
Talia-Hamid <taliahamid20@gmail.com>
2022-05-17 23:01:31 +0500

    sending OTP

--- app/src/main/java/com/example/esap/SplashScreen.java
+++ app/src/main/java/com/example/esap/SplashScreen.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -22,8 +26,19 @@ public class SplashScreen extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 finally {
-                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                    Log.i("currentUser", currentUser.toString());
+                    Intent intent;
+                    if(currentUser != null)
+                    {
+                        intent = new Intent(SplashScreen.this, MainActivity.class);
+                        // send user to mainactivity
+                    }
+                    else{
+                        intent = new Intent(SplashScreen.this, OTPActivity.class);
+                    }
                     startActivity(intent);
+
                 }
             }
         };
