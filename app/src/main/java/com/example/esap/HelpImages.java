14df33088088d604c9767b8ef5586ee4bceef64e
faizannsethi <faizansethi555@gmail.com>
2022-05-18 03:26:20 +0500

    Help images page done

--- app/src/main/java/com/example/esap/HelpImages.java
+++ app/src/main/java/com/example/esap/HelpImages.java
@@ -12,29 +12,29 @@ public class HelpImages extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
-        setContentView(R.layout.activity_help_images);
-        String service = intent.getStringExtra("key");
-        String[] splitService= service.split(" ");
-        Log.i("splitService", splitService[0] );
-        if(splitService[0].equals("firebrigade")){
-            ImageView imageView = (ImageView) findViewById(R.id.imageView12);
-            imageView.setImageResource(R.drawable.fire400);
-            ImageView imageView2 = (ImageView) findViewById(R.id.imageView19);
-            imageView2.setImageResource(R.drawable.burn400);
-            ImageView imageView3 = (ImageView) findViewById(R.id.imageView21);
-            imageView3.setImageResource(R.drawable.burn400);
-        }
-        else{
-            Log.i("splitService", splitService[0] );
-            ImageView imageView = (ImageView) findViewById(R.id.imageView12);
-            imageView.setImageResource(R.drawable.injury400);
-            ImageView imageView2 = (ImageView) findViewById(R.id.imageView19);
-            imageView2.setImageResource(R.drawable.heatstroke400);
-            ImageView imageView3 = (ImageView) findViewById(R.id.imageView21);
-            imageView3.setImageResource(R.drawable.snakebite);
-        }
+//        super.onCreate(savedInstanceState);
+//        Intent intent = getIntent();
+//        setContentView(R.layout.activity_help_images);
+//        String service = intent.getStringExtra("key");
+//        String[] splitService= service.split(" ");
+//        Log.i("splitService", splitService[0] );
+//        if(splitService[0].equals("firebrigade")){
+//            ImageView imageView = (ImageView) findViewById(R.id.imageView12);
+//            imageView.setImageResource(R.drawable.fire400);
+//            ImageView imageView2 = (ImageView) findViewById(R.id.imageView19);
+//            imageView2.setImageResource(R.drawable.burn400);
+//            ImageView imageView3 = (ImageView) findViewById(R.id.imageView21);
+//            imageView3.setImageResource(R.drawable.burn400);
+//        }
+//        else{
+//            Log.i("splitService", splitService[0] );
+//            ImageView imageView = (ImageView) findViewById(R.id.imageView12);
+//            imageView.setImageResource(R.drawable.injury400);
+//            ImageView imageView2 = (ImageView) findViewById(R.id.imageView19);
+//            imageView2.setImageResource(R.drawable.heatstroke400);
+////            ImageView imageView3 = (ImageView) findViewById(R.id.imageView21);
+////            imageView3.setImageResource(R.drawable.snakebite);
+//        }
 
     }
 }
