916b6122027c995c5f0355b9e6dd37af75252c2d
faizannsethi <faizansethi555@gmail.com>
2022-05-21 02:27:38 +0500

    App

--- app/src/main/java/com/example/esap/OTPActivity.java
+++ app/src/main/java/com/example/esap/OTPActivity.java
@@ -118,8 +118,8 @@ public class OTPActivity extends AppCompatActivity {
                             FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
                             String userPhoneNo = currentUser.getPhoneNumber();
                             addUserToDatabase(userPhoneNo, uid);
-                            if(userPhoneNo.equals("+923155903128")){
-                                FirebaseMessaging.getInstance().subscribeToTopic("ambulance")
+                            if(userPhoneNo.equals("+923036765805")){
+                                FirebaseMessaging.getInstance().subscribeToTopic("/topics/ambulance")
                                         .addOnCompleteListener(new OnCompleteListener<Void>() {
                                             @Override
                                             public void onComplete(@NonNull Task<Void> task) {
